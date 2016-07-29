5bef804b6478389c27da3f632b4d849ef75f262a
Lasse Meyer <meyer.lasse@gmail.com>
2016-07-29 23:28:53 +0200

    Remove unnecessary escapes for single quotes.

--- start.sh
+++ start.sh
@@ -206,7 +206,7 @@ setw -g window-status-fg black
 setw -g window-status-attr reverse
 
 # Info on left (I don't have a session display for now)
-set -g status-left \'\'
+set -g status-left ''
 
 # loud or quiet?
 set-option -g visual-activity off
@@ -238,20 +238,20 @@ set -g status-position bottom
 set -g status-bg colour234
 set -g status-fg colour137
 set -g status-attr dim
-set -g status-left \'\'
-set -g status-right \'#[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M:%S \'
+set -g status-left ''
+set -g status-right '#[fg=colour233,bg=colour241,bold] %d/%m #[fg=colour233,bg=colour245,bold] %H:%M:%S '
 set -g status-right-length 50
 set -g status-left-length 20
 
 setw -g window-status-current-fg colour81
 setw -g window-status-current-bg colour238
 setw -g window-status-current-attr bold
-setw -g window-status-current-format \' #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F \'
+setw -g window-status-current-format ' #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F '
 
 setw -g window-status-fg colour138
 setw -g window-status-bg colour235
 setw -g window-status-attr none
-setw -g window-status-format \' #I#[fg=colour237]:#[fg=colour250]#W#[fg=colour244]#F \'
+setw -g window-status-format ' #I#[fg=colour237]:#[fg=colour250]#W#[fg=colour244]#F '
 
 setw -g window-status-bell-attr bold
 setw -g window-status-bell-fg colour255
